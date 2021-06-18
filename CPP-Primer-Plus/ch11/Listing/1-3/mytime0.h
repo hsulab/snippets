658731a54c4e554600697f7185c88fa66cedc910
jyxu <ahcigar@foxmail.com>
2021-06-18 17:52:00 +0100

    start ch11

---
+++ CPP-Primer-Plus/ch11/Listing/1-3/mytime0.h
@@ -0,0 +1,24 @@
+// mytime0.h -- Time class before operator overloading 
+// Listing 11.1 page 566
+#ifndef MYTIME0_H_ 
+#define MYTIME0_H_
+
+class Time { 
+    
+    private:
+
+        int hours; 
+        int minutes; 
+    
+    public:
+
+        Time(); 
+        Time(int h, int m = 0); 
+        void AddMin(int m); 
+        void AddHr(int h); 
+        void Reset(int h = 0, int m = 0); 
+        Time Sum(const Time & t) const; 
+        void Show() const;
+
+}; 
+#endif
